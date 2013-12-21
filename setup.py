80951f8d74214ff40a88f2a3437fd669ac6c5ac8
Josh Austin <Josh.Austin@gmail.com>
2013-12-21 16:07:52 -0500

    added setup and requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='maasivepy',
+    version='1.0',
+    author='Josh Austin',
+    author_email='josh@maasive.net',
+    py_modules=['maasivepy'],
+)

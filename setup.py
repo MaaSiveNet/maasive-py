c3f8e3fd62615f9dbe83c0833fcdf7d95759dc72
Josh Austin <Josh.Austin@gmail.com>
2014-07-03 16:55:14 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='maasivepy',
-    version='1.2.1',
+    version='1.2.3',
     author='Josh Austin',
     author_email='josh@maasive.net',
     py_modules=['maasivepy'],

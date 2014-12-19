d9ab07dc715472386eed7062cc5aa22846c965cd
Timothy Van Heest <timothy@ionicsecurity.com>
2014-12-19 09:47:22 -0500

    Added MIT license. Also cleaned up requirements definition for installation.

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version='1.2.7',
     author='Josh Austin',
     author_email='josh@maasive.net',
+    license='MIT',
     packages=['maasivepy'],
+    install_requires=['requests>=1.2.3'],
     entry_points={
         "console_scripts": [
             "maasivecommand=maasivepy.maasivecommand:main"
